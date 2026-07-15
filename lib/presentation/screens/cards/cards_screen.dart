4486764c2497687433c493deb636e39662afa38b
Sylvie Salazar <sylvina_srosales@hotmail.com>
2026-07-15 14:55:22 -0600

    feat: cards with images

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -42,6 +42,19 @@ class _CardsView extends StatelessWidget {
               label: card['label'],
             ),
           ),
+          ...cards.map(
+            (card) => _CardTypeThree(
+              elevation: card['elevation'],
+              label: card['label'],
+            ),
+          ),
+          ...cards.map(
+            (card) => _CardTypeFour(
+              elevation: card['elevation'],
+              label: card['label'],
+            ),
+          ),
+          SizedBox(height: 100,)
         ],
       ),
     );
@@ -109,6 +122,74 @@ class _CardTypeTwo extends StatelessWidget {
             Align(alignment: AlignmentGeometry.bottomLeft, child: Text('$label - outline')),
           ],
         ),
+      ),
+    );
+  }
+}
+
+
+class _CardTypeThree extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardTypeThree({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+      color: colors.surfaceContainerHighest,
+      elevation: elevation,
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.more_vert_outlined),
+              ),
+            ),
+            Align(alignment: AlignmentGeometry.bottomLeft, child: Text('$label - filled')),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CardTypeFour extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardTypeFour({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Card(
+      clipBehavior: Clip.hardEdge,
+      elevation: elevation,
+      child: Stack(
+        children: [
+          Image.network('https://picsum.photos/id/${elevation.toInt()}/600/250', height: 200, fit: BoxFit.cover,),
+          Align(
+            alignment: Alignment.topRight,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20))
+              ),
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.more_vert_outlined),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
