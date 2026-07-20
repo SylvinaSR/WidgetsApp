3d249b4eee64ee3450d9a324ecae492c48e09a4f
Sylvie Salazar <sylvina_srosales@hotmail.com>
2026-07-20 16:27:56 -0600

    feat: riverpod - change values with riverpod

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgets_app/presentation/providers/counter_provider.dart';
+import 'package:widgets_app/presentation/providers/theme_provider.dart';
 
 class CounterScreen extends ConsumerWidget {
   static const String name = 'counter_screen';
@@ -10,15 +11,29 @@ class CounterScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final int clickCounter = ref.watch(counterProvider);
+    final bool isDarkMode = ref.watch(isDarkModeProvider);
 
     final titleStyle = Theme.of(context).textTheme.titleLarge;
 
     return Scaffold(
-      appBar: AppBar(title: Text('Counter Screen')),
+      appBar: AppBar(
+        title: Text('Counter Screen'),
+        actions: [
+          IconButton(
+            icon: isDarkMode ? Icon(Icons.dark_mode_outlined) : Icon(Icons.light_mode_outlined),
+            onPressed: () {
+              ref.read(isDarkModeProvider.notifier).update((state) => !state);
+            },
+          ),
+        ],
+      ),
       body: Center(child: Text('Value: $clickCounter', style: titleStyle)),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () {
+          //ref.read(counterProvider.notifier).state++; funciona igual que lo de abajo
+          ref.read(counterProvider.notifier).update((state) => state + 1);
+        },
       ),
     );
   }
